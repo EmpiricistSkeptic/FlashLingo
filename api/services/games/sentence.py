9dac63dd979710b50bdbd685dcec2f2203267901
stasstoun <stasstoun>
2026-09-23 18:30:45 +0000

    import fixed

--- api/services/games/sentence.py
+++ api/services/games/sentence.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 
 from ...models import Flashcard, GameAttempt
 
-from ..deepseek import (
+from ..deepseek_service import (
     AIServiceError,
     DeepSeekService,
 )
